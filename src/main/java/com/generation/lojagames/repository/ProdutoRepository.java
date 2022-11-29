644851e31f5e5097b94735c50f88b15ef21c05f9
VictorSa <victor.rsa@mercadolivre.com>
2022-11-29 08:35:10 -0300

    adicionando @Repository

--- src/main/java/com/generation/lojagames/repository/ProdutoRepository.java
+++ src/main/java/com/generation/lojagames/repository/ProdutoRepository.java
@@ -3,10 +3,13 @@ package com.generation.lojagames.repository;
 import com.generation.lojagames.model.Produtos;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.math.BigDecimal;
 import java.util.List;
 
+
+@Repository
 public interface ProdutoRepository extends JpaRepository<Produtos, Long> {
 
 
